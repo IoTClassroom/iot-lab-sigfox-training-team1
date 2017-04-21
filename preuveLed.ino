9d4bddbbbc2187fbc653237bb08ec19120e862c0
juancito94 <juanpbeloso@hotmail.com>
2017-04-21 11:14:06 +0200

    Add files via upload

---
+++ preuveLed.ino
@@ -0,0 +1,22 @@
+
+int PINLED = 5;
+int DELAY_BRIGHTNESS = 500;
+
+void setup() {
+  // initialize digital pin 5 as an output.
+  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  for(int i=0; i<=255;){
+    analogWrite(PINLED, i);
+    i+=5;
+    DELAY_BRIGHTNESS+=50;
+    delay(DELAY_BRIGHTNESS);
+    Serial.print("Brigtness = ");
+    Serial.println(i);
+    }
+
+}
